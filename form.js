35a10418265dba8d9787cbbdf9fabaa00465ed97
AshutoshAryan18 <72258793+AshutoshAryan18@users.noreply.github.com>
2021-02-04 20:00:18 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,34 @@
+class Form{
+    constructor(){
+        this.input=createInput("NAME")
+        this.button=createButton("PLAY")
+        this.greeting=createElement("h4")
+
+
+    }
+    hide(){
+        this.input.hide()
+        this.button.hide()
+        this.greeting.hide()
+
+    }
+    display(){
+        var title=createElement("h2")
+        title.html("3D BIKE RACING GAME")
+        title.position(displayWidth/2,100)
+        this.input.position(600,300)
+        this.button.position(600,400)
+
+       this.button.mousePressed(()=>{
+            this.input.hide()
+           this.button.hide()
+            player.name=this.input.value()
+            playercount+=1
+            player.index=playercount
+            player.update()
+            player.updatecount(playercount)
+            this.greeting.html("HI "+player.name)
+            this.greeting.position(100,200)
+        })
+    }
+}
